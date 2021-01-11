46292f9342c9ffa6b03cc9663e0242b018723111
balderfreir <71429565+balderfreir@users.noreply.github.com>
2021-01-11 22:30:56 +0400

    11.01.21

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def F2(x1, x2):
 
 def derivation(x, fun):
     h = 0.001
-    return round((fun(x + h) - fun(x)) / (h), 2)
+    return round((fun(x + h) - fun(x)) / h, 2)
 
 
 def gradient(point, fun):
@@ -27,7 +27,7 @@ def gradient(point, fun):
 def gradient_optimization_one_dim(x, fun):
     h = 0.001
     for i in range(0, 50):
-        x = x - h * ((fun(x + h) - fun(x)) / (h))
+        x = x - h * derivation(x, fun)
 
     return round(x, 2)
 
@@ -42,17 +42,8 @@ def gradient_optimization_multi_dim(point, fun):
     list = [round(x1, 2), round(x2, 2)]
     return list
 
-
-derivation(10, F1)
-
 point1 = [10, 1]
-gradient(point1, F2)
-
-gradient_optimization_one_dim(10, F1)
-
 point2 = [4, 10]
-gradient_optimization_multi_dim(point2, F2)
-
 print(derivation(10, F1))
 print(gradient(point1, F2))
 print(gradient_optimization_one_dim(10, F1))
