3b091afe80b333652ff95e91aa750202649911f3
balderfreir <71429565+balderfreir@users.noreply.github.com>
2021-01-12 21:28:46 +0400

    12.01.21

--- main.py
+++ main.py
@@ -19,20 +19,14 @@ def derivation(x, fun):
 def gradient(point, fun):
     x1 = point[0]
     x2 = point[1]
-    h = 0.0001
+    h = 0.01
     point_x1 = [x1 + h, x2]
-    point_x2 = [x1, x2 + h]
+    point_x2 = [x1, x2+h]
     x1 = (fun(point_x1) - fun(point)) / h
     x2 = (fun(point_x2) - fun(point)) / h
     list = [round(x1, 2), round(x2, 2)]
     return list
 
-
-# def gradient_optimization_one_dim(x, fun):
-#     h = 0.001
-#     for i in range(0, 50):
-#         x = x - h * (fun(x + h) - fun(x)) / h
-#     return round(x, 2)
 
 def gradient_optimization_one_dim(fun):
     h = 0.001
@@ -46,12 +40,14 @@ def gradient_optimization_multi_dim(fun):
     x1 = 4.0
     x2 = 10.0
     h = 0.001
-    point = [x1, x2]
-    point_x1 = [x1 + h, x2]
-    point_x2 = [x1, x2 + h]
+
     for i in range(0, 50):
-        x1 = x1 - h * (fun(point_x1) - fun(point)) / h
-        x2 = x2 - h * (fun(point_x2) - fun(point)) / h
+        point = [x1, x2]
+        point_x1 = [x1 + h, x2]
+        point_x2 = [x1, x2 + h]
+
+        x1 = x1 - h*((fun(point_x1)-fun(point)/h))
+        x2 = x2 - h*((fun(point_x2)-fun(point)/h))
 
     list = [round(x1, 2), round(x2, 2)]
     return list
@@ -59,7 +55,5 @@ def gradient_optimization_multi_dim(fun):
 
 point1 = [10, 1]
 point2 = [4, 10]
-print(derivation(10, F1))
 print(gradient(point1, F2))
-print(gradient_optimization_one_dim(F1))
 print(gradient_optimization_multi_dim(F2))
