fe6fdedcae27c5cd25677f32a68e5de2f54818c0
balderfreir <71429565+balderfreir@users.noreply.github.com>
2021-01-12 20:59:28 +0400

    12.01.21

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 import math
 
+
 def F1(x):
     return math.cos(x) + 0.05 * (x ** 3) + math.log2(x ** 2)
+
 
 def F2(point):
     x1 = point[0]
     x2 = point[1]
     return (x1 ** 2) * math.cos(x2) + 0.05 * (x2 ** 3) + math.log2(x2 ** 2)
 
+
 def derivation(x, fun):
     h = 0.0001
     return round((fun(x + h) - fun(x)) / h, 2)
+
 
 def gradient(point, fun):
     x1 = point[0]
     x2 = point[1]
     h = 0.0001
     point_x1 = [x1 + h, x2]
-    point_x2 = [x1, x2+h]
-    x1 = (fun(point_x1) - fun(point))/h
-    x2 = (fun(point_x2) - fun(point))/h
+    point_x2 = [x1, x2 + h]
+    x1 = (fun(point_x1) - fun(point)) / h
+    x2 = (fun(point_x2) - fun(point)) / h
     list = [round(x1, 2), round(x2, 2)]
     return list
+
 
 # def gradient_optimization_one_dim(x, fun):
 #     h = 0.001
@@ -36,16 +41,17 @@ def gradient_optimization_one_dim(fun):
         x = x - h * (fun(x + h) - fun(x)) / h
     return round(x, 2)
 
-def gradient_optimization_multi_dim(point, fun):
-    x1 = point[0]
-    x2 = point[1]
+
+def gradient_optimization_multi_dim(fun):
+    x1 = 4.0
+    x2 = 10.0
     h = 0.001
+    point = [x1, x2]
     point_x1 = [x1 + h, x2]
     point_x2 = [x1, x2 + h]
     for i in range(0, 50):
-
-        x1 = x1 - h * (fun(point_x1) - fun(point))/h
-        x2 = x2 - h * (fun(point_x2) - fun(point))/h
+        x1 = x1 - h * (fun(point_x1) - fun(point)) / h
+        x2 = x2 - h * (fun(point_x2) - fun(point)) / h
 
     list = [round(x1, 2), round(x2, 2)]
     return list
@@ -56,4 +62,4 @@ point2 = [4, 10]
 print(derivation(10, F1))
 print(gradient(point1, F2))
 print(gradient_optimization_one_dim(F1))
-print(gradient_optimization_multi_dim(point2, F2))
+print(gradient_optimization_multi_dim(F2))
