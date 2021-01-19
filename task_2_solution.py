543de83c64defc318784478f27ba9b0ea28e8161
balderfreir <71429565+balderfreir@users.noreply.github.com>
2021-01-19 18:10:36 +0400

    19.01.21

--- task_2_solution.py
+++ task_2_solution.py
@@ -2,37 +2,41 @@ import pandas as pd
 import numpy as np
 
 
-def calculate_data_shape(dt):
-    return print(dt.shape)
+def calculate_data_shape(dataframe):
+    return dataframe.shape
 
 
-def take_columns(dt):
-    return print(dt.columns)
+def take_columns(dataframe):
+    return dataframe.columns
 
 
 def calculate_target_ratio(dataframe, target_name):
-    mean = round(dataframe[target_name].mean(),2)
-    return print(mean)
+    return round(dataframe[target_name].mean(), 2)
 
 
-def calculate_data_dtypes(df):
-    return print(df.dtypes)
+def calculate_data_dtypes(dataframe):
+    return dataframe.dtypes
 
 
-def calculate_cheap_apartment():
-    pass
+def calculate_cheap_apartment(dataframe):
+    df = dataframe.loc[dataframe.loc[:,'price_doc'] <= 1000000 ]
+    return len(df.index)
 
 
-def calculate_squad_in_cheap_apartment():
-    pass
+def calculate_squad_in_cheap_apartment(dataframe):
+    df = dataframe.loc[dataframe.loc[:, 'price_doc'] <= 1000000]
+    return int(df['full_sq'].mean())
 
 
-def calculate_mean_price_in_new_housing():
-    pass
+def calculate_mean_price_in_new_housing(dataframe):
+    df = dataframe.loc[dataframe.loc[:, 'num_room'] == 3.0]
+    df = dataframe.loc[dataframe.loc[:, 'build_year'] >= 2010.0]
+    return int(df['price_doc'].mean())
 
 
-def calculate_mean_squared_by_num_rooms():
-    pass
+def calculate_mean_squared_by_num_rooms(dataframe):
+    dataframe['price_mean'] = round(dataframe['full_sq']/dataframe['num_room'],2)
+    return dataframe['price_mean']
 
 
 def calculate_squared_stats_by_material():
@@ -42,9 +46,15 @@ def calculate_squared_stats_by_material():
 def calculate_crosstab():
     pass
 
+df = pd.read_csv('sberbank_housing_market.csv', sep=',')
 
-# df = pd.read_csv('sberbank_housing_market.csv', sep=',')
-# calculate_data_shape(df)
-# take_columns(df)
-# calculate_target_ratio(df,'price_doc')
-# calculate_data_dtypes(df)
+# print(df.shape)
+# print(calculate_cheap_apartment(df))
+# print(calculate_squad_in_cheap_apartment(df))
+# print(calculate_mean_price_in_new_housing(df))
+# print(calculate_mean_squared_by_num_rooms(df))
+# print(take_columns(df))
+# # calculate_data_dtypes(df)
+# print(df['num_room'])
+# print(df['build_year'])
+# print(df['material'])
