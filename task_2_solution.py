7b8b4f79a8420ca4e835072955c7b9cf6a72ebf7
balderfreir <71429565+balderfreir@users.noreply.github.com>
2021-01-22 00:14:43 +0400

    22.01.21

--- task_2_solution.py
+++ task_2_solution.py
@@ -15,27 +15,37 @@ def calculate_target_ratio(dataframe, target_name):
 
 
 def calculate_data_dtypes(dataframe):
-    return dataframe.dtypes
+    cat_pri = 0
+    num_pri = 0
+    list_columns = dataframe.columns
+    for column in list_columns:
+        if dataframe[column].dtype == 'object':
+            cat_pri += 1
+        else:
+            num_pri += 1
+
+    return num_pri, cat_pri
 
 
 def calculate_cheap_apartment(dataframe):
-    df = dataframe.loc[dataframe.loc[:,'price_doc'] <= 1000000 ]
+    df = dataframe.loc[dataframe.loc[:, 'price_doc'] <= 1000000]
     return len(df.index)
 
 
 def calculate_squad_in_cheap_apartment(dataframe):
-    df = dataframe.loc[dataframe.loc[:, 'price_doc'] <= 1000000]
+    df = dataframe.loc[dataframe.loc[:, 'price_doc'] <= 1000000.0]
     return int(df['full_sq'].mean())
 
 
 def calculate_mean_price_in_new_housing(dataframe):
-    df = dataframe.loc[dataframe.loc[:, 'num_room'] == 3.0]
+
     df = dataframe.loc[dataframe.loc[:, 'build_year'] >= 2010.0]
+    df = dataframe.loc[dataframe.loc[:, 'num_room'] == 3.0]
     return int(df['price_doc'].mean())
 
 
 def calculate_mean_squared_by_num_rooms(dataframe):
-    dataframe['price_mean'] = round(dataframe['full_sq']/dataframe['num_room'],2)
+    dataframe['price_mean'] = round(dataframe['full_sq'] / dataframe['num_room'], 2)
     return dataframe['price_mean']
 
 
@@ -46,15 +56,16 @@ def calculate_squared_stats_by_material():
 def calculate_crosstab():
     pass
 
-# df = pd.read_csv('sberbank_housing_market.csv', sep=',')
 
-# print(df.shape)
+# df = pd.read_csv('sberbank_housing_market.csv', sep=',')
+# df = df.loc[df.loc[:, 'price_doc'] <= 1000000]
+# print(calculate_data_dtypes(df))
+# print(len(df.columns))
 # print(calculate_cheap_apartment(df))
 # print(calculate_squad_in_cheap_apartment(df))
 # print(calculate_mean_price_in_new_housing(df))
 # print(calculate_mean_squared_by_num_rooms(df))
 # print(take_columns(df))
-# # calculate_data_dtypes(df)
 # print(df['num_room'])
 # print(df['build_year'])
 # print(df['material'])
