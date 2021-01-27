cfbcf92e04c7329eb2891b8ec4ea8d0431aa557c
balderfreir <71429565+balderfreir@users.noreply.github.com>
2021-01-27 22:53:50 +0400

    27.01.21 Ad dataframe from csv

--- task_3_solution.py
+++ task_3_solution.py
@@ -2,29 +2,32 @@ import pandas as pd
 import numpy as np
 
 
-def split_data_into_two_samples():  # 1
+def split_data_into_two_samples(dataframe):  # 1
     pass
 
 
-def prepare_data():  # 2
+def prepare_data(dataframe):  # 2
     pass
 
 
-def scale_data():  # 3
+def scale_data(dataframe):  # 3
     pass
 
 
-def prepare_data_for_model():  # 4
+def prepare_data_for_model(dataframe):  # 4
     pass
 
 
-def fit_first_linear_model():  # 5, 6
+def fit_first_linear_model(dataframe):  # 5, 6
     pass
 
 
-def evaluate_model():  # 7
+def evaluate_model(dataframe):  # 7
     pass
 
 
-def calculate_model_weights():  # 8
+def calculate_model_weights(dataframe):  # 8
     pass
+
+
+df = pd.read_csv('sberbank_housing_market.csv', sep=',')
