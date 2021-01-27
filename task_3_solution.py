0979b6965306c1d6c3de33c2f77658b1846647f9
balderfreir <71429565+balderfreir@users.noreply.github.com>
2021-01-27 22:48:14 +0400

    27.01.21 Create file and all function (use 'pass')

---
+++ task_3_solution.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+
+
+def split_data_into_two_samples():  # 1
+    pass
+
+
+def prepare_data():  # 2
+    pass
+
+
+def scale_data():  # 3
+    pass
+
+
+def prepare_data_for_model():  # 4
+    pass
+
+
+def fit_first_linear_model():  # 5, 6
+    pass
+
+
+def evaluate_model():  # 7
+    pass
+
+
+def calculate_model_weights():  # 8
+    pass
