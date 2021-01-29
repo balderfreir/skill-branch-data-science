f040f2e09468c38e32ac394d849b0b3a02b63a74
balderfreir <71429565+balderfreir@users.noreply.github.com>
2021-01-29 21:35:30 +0400

    29.01.21 Make functional of 'split_data_into_two_samples'

--- task_3_solution.py
+++ task_3_solution.py
@@ -1,9 +1,24 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+# from mpl_toolkits.mplot3d import Axes3D
+# from matplotlib import cm
+# from copy import deepcopy
+# from tqdm import tqdm
+
+from sklearn.datasets import make_regression
+from sklearn.linear_model import SGDClassifier
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.model_selection import train_test_split
 
 
 def split_data_into_two_samples(dataframe):  # 1
-    pass
+    x_train, x_test = train_test_split(dataframe,test_size=70, train_size=30, random_state=42 )
+    return x_train, x_test
+
 
 
 def prepare_data(dataframe):  # 2
@@ -29,5 +44,5 @@ def evaluate_model(dataframe):  # 7
 def calculate_model_weights(dataframe):  # 8
     pass
 
-
-# df = pd.read_csv('sberbank_housing_market.csv', sep=',')
+df = pd.read_csv('sberbank_housing_market.csv', sep=',')
+print(split_data_into_two_samples(df))
