968b6e52fdf917540cb9ae187b5f9ce69390f35b
balderfreir <71429565+balderfreir@users.noreply.github.com>
2021-02-12 17:41:48 +0300

    delite columns whith NaN

--- task_3_solution.py
+++ task_3_solution.py
@@ -16,27 +16,32 @@ from sklearn.model_selection import train_test_split
 
 
 def split_data_into_two_samples(dataframe):  # 1
-     x_train, x_test = train_test_split(dataframe, test_size=0.3,
+    x_train, x_test = train_test_split(dataframe, test_size=0.3,
                                        random_state=42)
     # df_size = round(len(dataframe))
 
     # x = dataframe.head(int(df_size*0.7))
     # y = dataframe.tail(df_size - len(x))
 
-     return x_train, x_test
+    return x_train, x_test
 
 
 def prepare_data(dataframe):  # 2
-    df = dataframe.drop(['id'], axis='columns')
-    columns_list = df.columns
-    for el in columns_list:
-        if df[el].dtypes == 'object':
-            df = df.drop([el], axis='columns')
-
+    df = dataframe.select_dtypes(exclude='object')
     price_doc = df['price_doc']
     df = df.drop(['price_doc'], axis='columns')
+    df = df.drop(['id'], axis='columns')
     df = df.dropna()
-    # df = df.select_dtypes(exclude='object')
+    df = df.dropna(axis='columns', inplace=True)
+
+    # columns_list = df.columns
+    # for el in columns_list:
+    #     if df[el].dtypes == 'object':
+    #         df = df.drop([el], axis='columns')
+    #
+    # price_doc = df['price_doc']
+    # df = df.drop(['price_doc'], axis='columns')
+    #
     # print(df.dtypes)
     return price_doc, df
 
@@ -61,8 +66,8 @@ def calculate_model_weights(dataframe):  # 8
     pass
 
 
-# df = pd.read_csv('sberbank_housing_market.csv', sep=',')
+df = pd.read_csv('sberbank_housing_market.csv', sep=',')
 # print(split_data_into_two_samples(df))
-# print(prepare_data(df))
+print(prepare_data(df))
 
 # print(df['id'].dtypes)
